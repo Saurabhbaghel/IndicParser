59d36c8edee63829d376b587a12da2fae67d11ac
Saurabhbaghel <93883934+Saurabhbaghel@users.noreply.github.com>
2022-05-19 19:07:02 +0530

    made detectron2 installable

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
 from setuptools import setup
-from setuptools.command.install import _install
-import pip
-
-class install(_install):
-	def run(self):
-		_install.do_egg_install(self)
-		pip.main
-
 
 setup(
 	name = 'indicparser',
@@ -16,19 +8,14 @@ setup(
 # 	package_data = {
 # 		'configs':['*.yaml'],
 # 		},
-	dependency_links = [ 
-# 		'torchvision==0.6 -f '
-# 		'https://download.pytorch.org/whl/cu101/torch_stable.html',
-# 		'detectron2==0.1.3 -f' 
-		'https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/torch1.5/detectron2-0.1.3%2Bcu101-cp38-cp38-linux_x86_64.whl#egg=detectron2-0.1.3'
-		],
+
 		
 	
 		
 	install_requires = [
 		'numpy',
 		'opencv-python',
-# 		'torch==1.5', 
+		'torch==1.5', 
 		'torchvision==0.6 ', #'-f https://download.pytorch.org/whl/cu101/torch_stable.html',
 		'pyyaml==5.4',
 		'detectron2 @ https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/torch1.5/detectron2-0.1.3%2Bcu101-cp38-cp38-linux_x86_64.whl',
