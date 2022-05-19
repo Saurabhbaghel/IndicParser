1c415d7ef646f98706637fccd1ba1fb20a6a8622
Saurabhbaghel <93883934+Saurabhbaghel@users.noreply.github.com>
2022-05-19 16:56:26 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
-import setuptools
+from setuptools import setup
+from setuptools.command.install import _install
+import pip
 
-setuptools.setup(
+class install(_install):
+	def run(self):
+		_install.do_egg_install(self)
+		pip.main
+
+
+setup(
 	name = 'indicparser',
 	version = '0.1',
 	url = 'https://github.com/Saurabhbaghel/indicparser',
@@ -12,7 +20,7 @@ setuptools.setup(
 # 		'torchvision==0.6 -f '
 		'https://download.pytorch.org/whl/cu101/torch_stable.html',
 # 		'detectron2==0.1.3 -f' 
-		'https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/torch1.5/index.html'
+		'https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/torch1.5/detectron2-0.1.3%2Bcu101-cp38-cp38-linux_x86_64.whl'
 		],
 		
 	
