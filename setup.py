001f5ac4339e739dff7389310c23b7b78895ed02
Saurabhbaghel <93883934+Saurabhbaghel@users.noreply.github.com>
2022-05-19 17:11:03 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
 # 		'torch==1.5', 
 		'torchvision==0.6 ', #'-f https://download.pytorch.org/whl/cu101/torch_stable.html',
 		'pyyaml==5.4',
-# 		'detectron2==0.1.3', #'-f https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/torch1.5/index.html',
+		'detectron2==0.1.3 -f \ https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/torch1.5/index.html',
 		'pytesseract',
 		'pdf2image',
 		'pdfreader',
