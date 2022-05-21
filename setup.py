53b18a8a96f6277b5c733f350dcb9cd7f632f0e3
Saurabhbaghel <93883934+Saurabhbaghel@users.noreply.github.com>
2022-05-22 00:12:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,10 +16,10 @@ setup(
 	install_requires = [
 		'numpy',
 		'opencv-python',
-		'torch==1.5', 
-		'torchvision==0.6 ', #'-f https://download.pytorch.org/whl/cu101/torch_stable.html',
+# 		'torch==1.5', 
+# 		'torchvision==0.6 ', #'-f https://download.pytorch.org/whl/cu101/torch_stable.html',
 		'pyyaml==5.4',
-		'detectron2 @ git+https://github.com/facebookresearch/detectron2.git',
+# 		'detectron2 @ git+https://github.com/facebookresearch/detectron2.git',
 # 		https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/torch1.5/detectron2-0.1.3%2Bcu101-cp38-cp38-linux_x86_64.whl',
 		'pytesseract',
 		'pdf2image',
